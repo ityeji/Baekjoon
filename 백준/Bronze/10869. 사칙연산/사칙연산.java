35aa868545a64209db122724fd93944352e776c5
Jeong Yeji <110680436+ityeji@users.noreply.github.com>
2023-01-27 01:38:45 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+
+		if (num1 >= 1 && num2 <= 10000) {
+
+			System.out.println(num1 + num2);
+			System.out.println(num1 - num2);
+			System.out.println(num1 * num2);
+			System.out.println(num1 / num2);
+			System.out.println(num1 % num2);
+		}
+
+	}
+
+}
