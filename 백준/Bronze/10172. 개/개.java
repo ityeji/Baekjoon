708d40be3968ee30bf2875afea798fba3758b02e
Jeong Yeji <110680436+ityeji@users.noreply.github.com>
2023-01-29 09:15:35 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13924 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 백슬래시를 출력하려면 "\\"로 해줘야 백슬래시 하나 \가 출력되며,
+		// 백슬래시 두 개를 출력하고 싶으면 "\\\\"
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+
+	}
+
+}
