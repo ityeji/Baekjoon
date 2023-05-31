54ed410de2c6de7f7d57ee832b8d5e3350a8344f
Jeong Yeji <110680436+ityeji@users.noreply.github.com>
2023-05-31 14:18:16 +0900

    [Silver III] Title: 퇴사, Time: 116 ms, Memory: 12924 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -2,51 +2,41 @@ import java.util.Scanner;
 
 public class Main {
 
-	// 재귀로도 풀 수 있고
-	// DP로도 풀 수 있고!!!
-
-	// 이건 재귀 버전!
-
-	static int n;
-	static int[][] coun;
-	static int max; // 결과값
-
-	public static void DFS(int idx, int pay) {
-		if (idx >= n) {
-			max = Integer.max(max, pay);
-			return;
-		}
-
-		if (idx + coun[idx][0] <= n) { // 만약, 0번 인덱스(1일의 상담 일수)가 7이면 -> 간당하게 n일동안 상담함. 고로, 다음 상담은 진행할 수 없음.
-			// 그래서 idx+1을 안해도 되는 것
-			DFS(idx + coun[idx][0], pay + coun[idx][1]); // 지금까지 넘어온 금액에 지금 진행하는 상담의 금액 적기
-		} else {
-			DFS(idx + coun[idx][0], pay); // 만약, n을 넘어가!
-			// 그러면, 지금 걸려있는 상담은 진행 못하자나
-			// 금액은 넘겨주지 말고, 날짜만 넘겨줘서 기저조건에 걸리게 해 result 뽑아내자
-		}
-
-		// 지금 걸려있는 상담을 진행하지 않고, 다음 상담으로 넘어갈 수도 있잖아?
-		DFS(idx + 1, pay);
-	}
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		n = sc.nextInt();
+		int n = sc.nextInt();
 
-		coun = new int[n][2]; // 0열은 며칠걸리는지? 1열은 금액
+		// T는 5이하이므로 배열 크기를 넉넉하게 주기
+		// 일수
+		int[] day = new int[n + 6];
 
-		for (int i = 0; i < n; i++) {
-			coun[i][0] = sc.nextInt();
-			coun[i][1] = sc.nextInt();
+		// 금액
+		int[] pay = new int[n + 6];
+
+		// DP
+		// pay의 최대의 경우를 저장하는 배열
+		int[] dp = new int[n + 6];
+
+		for (int i = 1; i <= n; i++) {
+			day[i] = sc.nextInt();
+			pay[i] = sc.nextInt();
 		}
 
-		max = 0;
+		// 뒤에서부터 DP 만들어주기
 
-		DFS(0, 0); // 1일(idx로 접근할 거니까, 0번 인덱스 == 1일), 금액 0원부터 시작
+		for (int i = n; i > 0; i--) {
+			if (i + day[i] > n + 1) {
+				// i번째 날 상담을 시작해버리면, n을 넘어버릴 때
+				dp[i] = dp[i + 1];
+				// 어차피 i번째날 상담은 못잡으니까
+				// 뒷날의 최대값이 곧 i번째날의 최대값
+			} else {
+				dp[i] = Math.max(dp[i + 1], pay[i] + dp[i + day[i]]); // max값 갱신해서 넣어주기
+			}
+		}
 
-		System.out.println(max);
+		System.out.println(dp[1]); // 1일부터 n일까지의 최대값
 
 	}
 
