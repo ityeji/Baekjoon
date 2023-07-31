0d62ea26184f4d909ea13256bae633356e11f26b
Jeong Yeji <110680436+ityeji@users.noreply.github.com>
2023-08-01 01:07:49 +0900

    [Bronze V] Title: A+B - 7, Time: 132 ms, Memory: 13116 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+			System.out.println("Case #" + i + ": " + (a + b));
+		}
+	}
+
+}
