f76ea72c5844f5e1dab615412290204aed7cb84d
Jeong Yeji <110680436+ityeji@users.noreply.github.com>
2023-08-04 23:32:27 +0900

    [Bronze V] Title: A+B - 8, Time: 136 ms, Memory: 13128 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String args[]) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int T = in.nextInt();
+ 
+		for (int i = 1; i <= T; i++) {
+			int A = in.nextInt();
+			int B = in.nextInt();
+ 
+			System.out.println("Case #" + i + ": " + A + " + " + B + " = " + (A+B));
+		}
+ 
+		in.close();
+	}
+}
