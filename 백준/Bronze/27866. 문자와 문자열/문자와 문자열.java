02a02cd344d791d5b7e81aaeb26691413618b3dc
Jeong Yeji <110680436+ityeji@users.noreply.github.com>
2023-08-06 09:28:02 +0900

    [Bronze V] Title: 문자와 문자열, Time: 116 ms, Memory: 12880 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String s = sc.next();
+
+		int idx = sc.nextInt();
+
+		System.out.println(s.charAt(idx - 1));
+
+	}
+
+}
